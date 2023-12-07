54df20fb085178c6756a3a948dad68cd54e90749
nc-alex-davis <alex.davis@techreturners.com>
2023-12-07 09:21:40 +0000

    refactor: update instructions for PoemsList

--- src/components/poems_list.tsx
+++ src/components/poems_list.tsx
@@ -1,7 +1,17 @@
-interface PoemsListProps {}
+import { PoemsResponse } from "./poem_container"
+
+interface PoemsListProps {
+  poems: PoemsResponse
+}
 
 export const PoemsList: React.FC<PoemsListProps> = () => {
   // receive the data on props and map over it here
   // you can use the Poem component for each item in the list
-  return <p>This is the list</p>
+  return (
+    <>
+      <p>
+        This is where you can map over the data to render a Poem for each item
+      </p>
+    </>
+  )
 }
